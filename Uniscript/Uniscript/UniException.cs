eaf2e27d82994618c14847ba471eb7e0ed51464e
Christian Gunderman <gundermanc@gmail.com>
2015-09-04 16:30:07 -0400

    Symbol table interface and exception base.

---
+++ Uniscript/Uniscript/UniException.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="UniException.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author> 
+//-----------------------------------------------------------------------
+
+namespace Uniscript
+{
+    using System;
+
+    /// <summary>
+    /// The parent class for all publicly emitted exceptions that occur under
+    /// normal operation.
+    /// </summary>
+    public abstract class UniException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="inner">The inner exception that caused this exception.</param>
+        protected UniException(string message = null, Exception inner = null) : base(message, inner)
+        {
+        }
+    }
+}
