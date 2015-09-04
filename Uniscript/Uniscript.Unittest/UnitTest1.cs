bee233bf6c88280afdf56cce99ec18db2e609acb
Christian Gunderman <gundermanc@gmail.com>
2015-09-04 14:10:32 -0400

    Committed empty projects.

---
+++ Uniscript/Uniscript.Unittest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Uniscript.Unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
