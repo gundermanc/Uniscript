96a01e8254a4f66e812e99c05025fe5f6ecbfc83
Christian Gunderman <gundermanc@gmail.com>
2015-09-06 11:38:52 -0400

    Defined UniToken interface and UniTokenType enum.

---
+++ Uniscript/Uniscript/Tokens/IUniToken.cs
@@ -0,0 +1,26 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="IUniToken.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author> 
+//-----------------------------------------------------------------------
+
+namespace Uniscript.Tokens
+{
+    /// <summary>
+    /// The interface for all tokens.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value stored within this token.</typeparam>
+    public interface IUniToken<TValue>
+    {
+        /// <summary>
+        /// Gets a value indicating the token type.
+        /// </summary>
+        UniTokenType Type { get; }
+
+        /// <summary>
+        /// Gets the value of the token.
+        /// </summary>
+        TValue Value { get; }
+    }
+}
---
+++ Uniscript/Uniscript/Tokens/UniTokenType.cs
@@ -0,0 +1,20 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="UniTokenType.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author> 
+//-----------------------------------------------------------------------
+
+namespace Uniscript.Tokens
+{
+    /// <summary>
+    /// Enumeration of all possible token types.
+    /// </summary>
+    public enum UniTokenType
+    {
+        /// <summary>
+        /// Marks comment tokens.
+        /// </summary>
+        Comment
+    }
+}
