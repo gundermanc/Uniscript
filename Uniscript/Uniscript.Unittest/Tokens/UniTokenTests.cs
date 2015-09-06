7e1086ca2ec286df5d8bad3ef5e45b5a384eff6f
Christian Gunderman <gundermanc@gmail.com>
2015-09-06 11:49:43 -0400

    UniToken unittests.

---
+++ Uniscript/Uniscript.Unittest/Tokens/UniTokenTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uniscript.Tokens;
+
+namespace Uniscript.Unittest.Tokens
+{
+    /// <summary>
+    /// Token class unittests.
+    /// </summary>
+    [TestClass]
+    public class UniTokenTests
+    {
+        private const string STR_1 = "string 1";
+
+        /// <summary>
+        /// Checks that values are stored and can be accessed appropriately.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("UniToken")]
+        public void Constructor_ValuesGiven_AreStored()
+        {
+            var result = new UniToken<string>(UniTokenType.Comment, STR_1);
+
+            result.Type.Should().Be(UniTokenType.Comment, "the token type is given in the constructor");
+            result.Value.Should().Be(STR_1, "string was given in the constructor");
+        }
+    }
+}
---
+++ Uniscript/Uniscript/Tokens/UniToken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniscript.Tokens
+{
+    public class UniToken<TValue> : IUniToken<TValue>
+    {
+        public UniToken(UniTokenType type, TValue value)
+        {
+
+        }
+
+        public UniTokenType Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public TValue Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
