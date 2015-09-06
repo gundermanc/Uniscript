1be53c795bb8d66e0ad2676ed8f9d5a9e6ba3726
Christian Gunderman <gundermanc@gmail.com>
2015-09-06 11:56:31 -0400

    Token implementation.

--- Uniscript/Uniscript/Tokens/UniToken.cs
+++ Uniscript/Uniscript/Tokens/UniToken.cs
@@ -1,31 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//----------------------------------------------------------------------- 
+// <copyright file="UniToken.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author> 
+//-----------------------------------------------------------------------
 
 namespace Uniscript.Tokens
 {
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Basic token class implementation.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value stored within.</typeparam>
     public class UniToken<TValue> : IUniToken<TValue>
     {
+        /// <summary>
+        /// Stores the type readonly.
+        /// </summary>
+        private readonly UniTokenType type;
+
+        /// <summary>
+        /// Stores the value readonly.
+        /// </summary>
+        private readonly TValue value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniToken{TValue}"/>
+        /// </summary>
+        /// <param name="type">The type of the token.</param>
+        /// <param name="value">The value stored within the token.</param>
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", 
+            "SA1642:ConstructorSummaryDocumentationMustBeginWithStandardText",
+            Justification = "StyleCop is incorrectly reading the text here.")]
         public UniToken(UniTokenType type, TValue value)
         {
-
+            this.type = type;
+            this.value = value;
         }
 
+        /// <summary>
+        /// Gets the token's type.
+        /// </summary>
         public UniTokenType Type
         {
             get
             {
-                throw new NotImplementedException();
+                return this.type;
             }
         }
 
+        /// <summary>
+        /// Gets the token's value.
+        /// </summary>
         public TValue Value
         {
             get
             {
-                throw new NotImplementedException();
+                return this.value;
             }
         }
     }
