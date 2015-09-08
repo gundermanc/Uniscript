b8267602f09caf131b6979ce7608146c70dbdcd3
Christian Gunderman <gundermanc@gmail.com>
2015-09-08 18:06:20 -0400

    Basic lexer interface.

---
+++ Uniscript/Uniscript/IUniLexer.cs
@@ -0,0 +1,25 @@
+﻿//----------------------------------------------------------------------- 
+// <copyright file="IUniLexer.cs" company="Gundersoft"> 
+//     Copyright (c) Christian Gunderman. All rights reserved. 
+// </copyright> 
+// <author>Christian Gunderman</author> 
+//-----------------------------------------------------------------------
+
+namespace Uniscript
+{
+    using System.Collections.Generic;
+    using Tokens;
+
+    /// <summary>
+    /// Interface for lexer.
+    /// </summary>
+    public interface IUniLexer
+    {
+        /// <summary>
+        /// Explodes a string of code into a sequence of tokens.
+        /// </summary>
+        /// <param name="code">A piece of code.</param>
+        /// <returns>A sequence of tokens.</returns>
+        IEnumerable<IUniToken> Tokenize(string code);
+    }
+}
